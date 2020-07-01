b2e839068ac987efd24bd31e97f4a4652d629ca9
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-07-01 15:02:48 +0800

    task_receive & task_send: send and receive, without reply

--- src/intertask_communication/task_receive.cpp
+++ src/intertask_communication/task_receive.cpp
@@ -1,12 +1,64 @@
+#include <sys/mman.h>
+
+#include <iostream>
+
 #include <alchemy/task.h>
 
 #include <RtMacro.h>
 
 RT_TASK rtReceiveTask;
+RT_TASK rtSendTask;
+
+void ReceiveRoutine(void*)
+{
+  rt_printf("Waiting for messages\n");
+
+  for (;;)
+  {
+    RT_TASK_MCB receiveMessage;
+
+    receiveMessage.data = (char*) malloc(RtMacro::kMessageSize);
+    receiveMessage.size = RtMacro::kMessageSize;
+
+    rt_task_receive(&receiveMessage, TM_INFINITE);
+
+    rt_printf("message received: %s\n", receiveMessage.data);
+
+    RT_TASK_MCB sendMessage;
+
+    sendMessage.data = (char*) malloc(RtMacro::kMessageSize);
+    char sendData[] = "ack";
+    memcpy(sendMessage.data, sendData, sizeof(char) * RtMacro::kMessageSize);
+    sendMessage.size = RtMacro::kMessageSize;
+
+    rt_printf("Sending back reply: %s\n", sendMessage.data);
+
+    rt_task_reply(receiveMessage.flowid, &sendMessage);
+
+    rt_task_bind(&rtSendTask, "rtSendTask", TM_INFINITE);
+    rt_task_join(&rtSendTask);
+
+    rt_task_wait_period(NULL);
+
+    free(receiveMessage.data);
+    free(sendMessage.data);
+  }
+}
 
 int main(int argc, char *argv[])
 {
+  mlockall(MCL_CURRENT|MCL_FUTURE);
+
   rt_task_create(&rtReceiveTask, "rtReceiveTask", RtMacro::kStackSize,
-    RtMacro::kPriority, RtMacro::kMode);
+    RtMacro::kPriority, T_JOINABLE);
+  rt_task_set_periodic(&rtReceiveTask, TM_NOW,
+    rt_timer_ns2ticks(RtMacro::kOneHundredMilliseconds));
+  rt_task_start(&rtReceiveTask, ReceiveRoutine, NULL);
+
+  rt_task_join(&rtReceiveTask);
+
+  for (;;)
+  {}
+
   return 0;
 }
--- src/intertask_communication/task_send.cpp
+++ src/intertask_communication/task_send.cpp
@@ -1,17 +1,53 @@
 #include <sys/mman.h>
 
+#include <iostream>
+
 #include <alchemy/task.h>
 
 #include <RtMacro.h>
 
 RT_TASK rtSendTask;
+RT_TASK rtReceiveTask;
+
+RT_TASK_MCB receiveMessage;
+
+void SendRoutine(void*)
+{
+  rt_task_bind(&rtReceiveTask, "rtReceiveTask", TM_INFINITE);
+
+  RT_TASK_MCB sendMessage;
+
+  sendMessage.data = (char*) malloc(RtMacro::kMessageSize);
+  char sendData[] = "hello";
+  memcpy(sendMessage.data, sendData, sizeof(char) * RtMacro::kMessageSize);
+  sendMessage.size = RtMacro::kMessageSize;
+
+  receiveMessage.data = (char*) malloc(RtMacro::kMessageSize);
+  receiveMessage.size = RtMacro::kMessageSize;
+
+  auto retval = rt_task_send(&rtReceiveTask, &sendMessage, &receiveMessage, TM_INFINITE);
+
+  if (retval < 0)
+  {
+    rt_printf("Error with rt_task_send: %s\n", strerror(-retval));
+  }
+
+  rt_printf("Received reply: %s\n", receiveMessage.data);
+
+  // clean up
+  free(receiveMessage.data);
+  free(sendMessage.data);
+}
 
 int main(int argc, char *argv[])
 {
   mlockall(MCL_CURRENT|MCL_FUTURE);
 
   rt_task_create(&rtSendTask, "rtSendTask", RtMacro::kStackSize,
-    RtMacro::kPriority, RtMacro::kMode);
+    RtMacro::kPriority, T_JOINABLE);
+  rt_task_start(&rtSendTask, SendRoutine, NULL);
+
+  rt_task_join(&rtSendTask);
 
   return 0;
 }
