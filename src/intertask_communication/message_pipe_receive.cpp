4c78cba32f13911beddd22d23da292748442a329
Dennis Lu <Dennis.Lu@itri.org.tw>
2020-07-02 09:33:20 +0800

    message_pipe: able to send from linux process to xenomai task

--- src/intertask_communication/message_pipe_receive.cpp
+++ src/intertask_communication/message_pipe_receive.cpp
@@ -1,4 +1,64 @@
+#include <signal.h>
+
+#include <iostream>
+
+#include <alchemy/task.h>
+#include <alchemy/pipe.h>
+
+#include <RtMacro.h>
+
+RT_TASK rtReadTask;
+RT_TASK rtWriteTask;
+
+RT_PIPE rtPipe;
+
+void terminationHandler(int signal)
+{
+  rt_pipe_delete(&rtPipe);
+  exit(1);
+}
+
+void ReadRoutine(void*)
+{
+  void *buffer;
+  buffer = malloc(RtMacro::kMessageSize);
+
+  rt_printf("Reading from rtPipe\n");
+
+  auto retval = rt_pipe_read(&rtPipe, buffer, RtMacro::kMessageSize, TM_INFINITE);
+  if (retval < 0)
+  {
+    rt_printf("rt_pipe_read error: %s\n", strerror(-retval));
+  }
+
+  rt_printf("read pipe into buffer: %s\n", buffer);
+
+  free(buffer);
+}
+
 int main(int argc, char *argv[])
 {
+  struct sigaction action;
+  action.sa_handler = terminationHandler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  sigaction(SIGINT, &action, NULL);
+
+  auto retval = rt_pipe_create(&rtPipe, "rtPipe", 0, RtMacro::kQueuePoolSize);
+  if (retval < 0)
+  {
+    rt_printf("rt_pipe_create error: %s\n", strerror(-retval));
+  }
+  auto minor_number = retval;
+  rt_printf("/dev/rtp%d pipe is opened\n", minor_number);
+
+  rt_task_create(&rtReadTask, "rtReadTask", RtMacro::kStackSize,
+    RtMacro::kPriority, T_JOINABLE);
+  rt_task_start(&rtReadTask, ReadRoutine, NULL);
+
+  rt_task_join(&rtReadTask);
+
+  rt_pipe_delete(&rtPipe);
+
   return 0;
 }
--- src/intertask_communication/message_pipe_send.cpp
+++ src/intertask_communication/message_pipe_send.cpp
@@ -1,4 +1,45 @@
+#include <cstdlib>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <iostream>
+
+#include <alchemy/task.h>
+#include <alchemy/pipe.h>
+
+#include <RtMacro.h>
+
+RT_TASK rtWriteTask;
+
+RT_PIPE rtPipe;
+
 int main(int argc, char *argv[])
 {
+  const char device[] = "/dev/rtp0";
+
+  auto file_descriptor = open(device, O_WRONLY, S_IRWXG);
+
+  if (file_descriptor < 0)
+  {
+    printf("file descriptor error: %s\n", strerror(errno));
+  }
+
+  void *buffer;
+  buffer = malloc(RtMacro::kMessageSize);
+  char data[] = "hihihi";
+  memcpy(buffer, data, RtMacro::kMessageSize);
+
+  auto num_written = write(file_descriptor, buffer, RtMacro::kMessageSize);
+  printf("Written %ld bytes(?) into pipe, %s\n", num_written, device);
+
+  if (num_written < 0)
+  {
+    printf("write error: %s\n", strerror(errno));
+  }
+
+  free(buffer);
+  close(file_descriptor);
+
   return 0;
 }
